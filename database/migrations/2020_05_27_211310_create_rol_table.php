26112e13864143175b50c6d2d730ac49fa6d39e5
Alexander563-art <alexml0720@gmail.com>
2020-06-01 11:00:17 -0500

    se crearon las migraciones

--- database/migrations/2020_05_27_211310_create_rol_table.php
+++ database/migrations/2020_05_27_211310_create_rol_table.php
@@ -14,8 +14,11 @@ class CreateRolTable extends Migration
     public function up()
     {
         Schema::create('rol', function (Blueprint $table) {
+
             $table->id();
             $table->string('nombre', 50);
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_spanish_ci';
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'InicioController@index');
+
+route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    route::get('permiso', 'PermisoController@index')->name('permiso');
+    route::get('crear', 'PermisoController@crear')->name('crear_permiso');
 });
